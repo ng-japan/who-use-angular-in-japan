61670f2c39cc5c0fc620b38f5ffdab368da63332
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-12-30 03:00:51 +0000

    update side packages to v8

--- src/app/config/material.module.ts
+++ src/app/config/material.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MatCardModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const modules = [MatCardModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule];
 
